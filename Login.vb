ab02bbd38825ca192d58cf9e5c435a7070884f36
i-naeem <inaeem@protonmail.com>
2022-05-28 20:46:04 +0500

    :goal_net: check for empty username and password

--- Login.vb
+++ Login.vb
@@ -1,6 +1,17 @@
 ﻿Public Class Login
     Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
-        Me.Hide()
-        Dashboard.Show()
+        Dim username As String = UsernameTextBox.Text
+        Dim password As String = PasswordTextBox.Text
+
+        ' Check if password or username is empty give error
+        If String.IsNullOrEmpty(username) Or String.IsNullOrEmpty(password) Then
+            MessageBox.Show("Username or email can not be empty.")
+        Else
+            Me.Hide()
+            ' Reset the username and password input.
+            UsernameTextBox.Text = ""
+            PasswordTextBox.Text = ""
+            Dashboard.Show()
+        End If
     End Sub
 End Class
