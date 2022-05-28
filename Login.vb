07db8db4a945e09d8c4606c059509808187bd4cf
i-naeem <inaeem@protonmail.com>
2022-05-28 21:12:21 +0500

    :sparkles: connect database

--- Login.vb
+++ Login.vb
@@ -13,17 +13,30 @@ Public Class Login
         If String.IsNullOrEmpty(username) Or String.IsNullOrEmpty(password) Then
             MessageBox.Show("Username or email can not be empty.")
         Else
+            ' Creates a query to select user.
+            Dim query As String = "SELECT * FROM users_table WHERE username = '" + username + "' AND password = '" + password + "'"
+
+            Dim cmd As New SqlDataAdapter(query, con)
+            Dim dt As New DataTable()
+
+            ' Fill the data table with results from database
+            cmd.Fill(dt)
+
             ' Check if user exist with given username and password
-            Dim query As String = "SELECT user FROM users_table WHERE username = " + username + " AND password = " + password
-            Dim cmd As SqlDataAdapter = New SqlDataAdapter(query, con)
+            If dt.Rows.Count > 0 Then
+                ' Hide the login form
+                Me.Hide()
 
-            '
-            Me.Hide()
+                ' Reset the username and password input.
+                UsernameTextBox.Text = ""
+                PasswordTextBox.Text = ""
 
-            ' Reset the username and password input.
-            UsernameTextBox.Text = ""
-            PasswordTextBox.Text = ""
-            Dashboard.Show()
+                ' Show the dashboard form
+                Dashboard.Show()
+            Else
+                MessageBox.Show("Username or password is incorrect")
+            End If
+
         End If
     End Sub
 End Class
