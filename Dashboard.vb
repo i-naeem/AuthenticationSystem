686681acb28c64f3ba549f371e56628cbf8c26e7
i-naeem <inaeem@protonmail.com>
2022-05-28 20:27:16 +0500

    :sparkles: add functionality to dashboard page

--- Dashboard.vb
+++ Dashboard.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Dashboard
 
+
+    Private Sub Dashboard_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Application.Exit()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Login.Show()
+    End Sub
 End Class
