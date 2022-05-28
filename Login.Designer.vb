0d167a7cd751bb0908839efe69764678a02b126b
i-naeem <inaeem@protonmail.com>
2022-05-28 20:15:47 +0500

    :art: Setup login page layout

--- Login.Designer.vb
+++ Login.Designer.vb
@@ -22,20 +22,95 @@ Partial Class Login
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.UsernameTextBox = New System.Windows.Forms.TextBox()
+        Me.WelcomeLabel = New System.Windows.Forms.Label()
+        Me.PasswordTextBox = New System.Windows.Forms.TextBox()
+        Me.PasswordLabel = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.AutoSize = True
+        Me.UsernameLabel.Font = New System.Drawing.Font("Fira Code Medium", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.UsernameLabel.Location = New System.Drawing.Point(321, 221)
+        Me.UsernameLabel.Margin = New System.Windows.Forms.Padding(0)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(141, 29)
+        Me.UsernameLabel.TabIndex = 1
+        Me.UsernameLabel.Text = "Username"
+        '
+        'UsernameTextBox
+        '
+        Me.UsernameTextBox.Font = New System.Drawing.Font("Fira Code Medium", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.UsernameTextBox.Location = New System.Drawing.Point(328, 253)
+        Me.UsernameTextBox.Name = "UsernameTextBox"
+        Me.UsernameTextBox.Size = New System.Drawing.Size(524, 37)
+        Me.UsernameTextBox.TabIndex = 2
+        '
+        'WelcomeLabel
+        '
+        Me.WelcomeLabel.AutoSize = True
+        Me.WelcomeLabel.Font = New System.Drawing.Font("Fira Code", 47.99999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.WelcomeLabel.Location = New System.Drawing.Point(482, 98)
+        Me.WelcomeLabel.Name = "WelcomeLabel"
+        Me.WelcomeLabel.Size = New System.Drawing.Size(229, 79)
+        Me.WelcomeLabel.TabIndex = 0
+        Me.WelcomeLabel.Text = "Login"
+        '
+        'PasswordTextBox
+        '
+        Me.PasswordTextBox.Font = New System.Drawing.Font("Fira Code Medium", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.PasswordTextBox.Location = New System.Drawing.Point(328, 385)
+        Me.PasswordTextBox.Name = "PasswordTextBox"
+        Me.PasswordTextBox.Size = New System.Drawing.Size(524, 37)
+        Me.PasswordTextBox.TabIndex = 4
+        '
+        'PasswordLabel
+        '
+        Me.PasswordLabel.AutoSize = True
+        Me.PasswordLabel.Font = New System.Drawing.Font("Fira Code Medium", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.PasswordLabel.Location = New System.Drawing.Point(321, 353)
+        Me.PasswordLabel.Margin = New System.Windows.Forms.Padding(0)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(141, 29)
+        Me.PasswordLabel.TabIndex = 3
+        Me.PasswordLabel.Text = "Password"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Fira Code Medium", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(328, 493)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(535, 45)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1149, 625)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PasswordTextBox)
+        Me.Controls.Add(Me.PasswordLabel)
+        Me.Controls.Add(Me.UsernameTextBox)
+        Me.Controls.Add(Me.UsernameLabel)
+        Me.Controls.Add(Me.WelcomeLabel)
         Me.Name = "Login"
         Me.Opacity = 0.95R
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Login - Authentication System"
-        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-
+    Friend WithEvents UsernameLabel As Label
+    Friend WithEvents UsernameTextBox As TextBox
+    Friend WithEvents WelcomeLabel As Label
+    Friend WithEvents PasswordTextBox As TextBox
+    Friend WithEvents PasswordLabel As Label
+    Friend WithEvents Button1 As Button
 End Class
