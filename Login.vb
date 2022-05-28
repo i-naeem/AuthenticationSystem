c695996414b77967eb8c4963945efb4f87b90429
i-naeem <inaeem@protonmail.com>
2022-05-28 21:45:29 +0500

    :recycle: login event

--- Login.vb
+++ Login.vb
@@ -9,34 +9,36 @@ Public Class Login
         Dim username As String = UsernameTextBox.Text
         Dim password As String = PasswordTextBox.Text
 
-        ' Check if password or username is empty give error
+        'Check if password or username is empty give error
         If String.IsNullOrEmpty(username) Or String.IsNullOrEmpty(password) Then
             MessageBox.Show("Username or email can not be empty.")
-        Else
-            ' Creates a query to select user.
-            Dim query As String = "SELECT * FROM users_table WHERE username = '" + username + "' AND password = '" + password + "'"
-
-            Dim cmd As New SqlDataAdapter(query, con)
-            Dim dt As New DataTable()
-
-            ' Fill the data table with results from database
-            cmd.Fill(dt)
-
-            ' Check if user exist with given username and password
-            If dt.Rows.Count > 0 Then
-                ' Hide the login form
-                Me.Hide()
-
-                ' Reset the username and password input.
-                UsernameTextBox.Text = ""
-                PasswordTextBox.Text = ""
-
-                ' Show the dashboard form
-                Dashboard.Show()
-            Else
-                MessageBox.Show("Username or password is incorrect")
-            End If
-
+            Return
         End If
+
+        'Creates a query to select user.
+        Dim query As String = "SELECT * FROM users_table WHERE username = '" + username + "' AND password = '" + password + "'"
+
+        Dim cmd As New SqlDataAdapter(query, con)
+        Dim dt As New DataTable()
+
+        'Fill the data table with results from database
+        cmd.Fill(dt)
+
+        'Check if user exist with given username and password
+        If dt.Rows.Count = 0 Then
+            MessageBox.Show("Incorrect Username or password.")
+            Return
+        End If
+
+        'Hide the login form
+        Me.Hide()
+
+        'Reset the username and password input.
+        UsernameTextBox.Text = ""
+        PasswordTextBox.Text = ""
+
+        'Show the dashboard form
+        Dashboard.Show()
+
     End Sub
 End Class
